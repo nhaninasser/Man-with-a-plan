6334c780db8cb92efeacec7892059ff352e7e00b
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-10 22:39:03 -0400

    added idbjs file and linked it to html

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,59 @@
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+
+    db.createObjectStore("new_tracker", { autoIncrement: true });
+};
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) { }
+};
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveTracker(tracker) {
+    const transaction = db.transaction(["new_tracker"], "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_tracker");
+    budgetObjectStore.add(tracker);
+}
+
+function uploadTransaction() {
+    const transaction = db.transaction(["new_tracker"], "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_tracker");
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((serverResponse) => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(["new_tracker"], "readwrite");
+                    const budgetObjectStore = transaction.objectStore("new_tracker");
+
+                    budgetObjectStore.clear();
+
+                    alert("All saved transactions has been submitted!");
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+
+window.addEventListener("online", uploadTransaction);
