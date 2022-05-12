e7e4b0c7358bbcdb1bc26647df93faeceb6b95db
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-11 23:21:34 -0400

    fixed file structures trying to fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,63 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require("webpack")
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 module.exports = {
-    entry: "./Develop/public/js/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    mode: "development",
-}
+  entry: {
+    app: "./public/js/index.js",    
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/');
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "budget-app",
+      short_name: "budget-app",
+      description: "Offline budget tracker",
+      start_url: "./public/index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("public/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],  
+  mode: "development"
+};
