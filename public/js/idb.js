e67abad557a8712b23037a2f9c7226c87d6b38c4
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-12 19:19:25 -0400

    fixed manifest serviceworker and paths

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,57 +2,58 @@ let db;
 
 const request = indexedDB.open('budget_tracker', 1);
 
-request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    db.createObjectStore('new_tracker', { autoIncrement: true });
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore('new_tracker', { autoIncrement: true });
 };
 
-request.onsuccess = function (event) {
-    db = event.target.result;
-    if (navigator.onLine) {
-    }
+request.onsuccess = function(event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    // uploadTracker();
+  }
 };
 
-request.onerror = function (event) {
-    console.log(event.target.errorCode);
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_tracker'], 'readwrite');
-    const trackerObjectStore = transaction.objectStore('new_tracker');
-    trackerObjectStore.add(record);
+  const transaction = db.transaction(['new_tracker'], 'readwrite');
+  const trackerObjectStore = transaction.objectStore('new_tracker');
+  trackerObjectStore.add(record);
 }
 
 function uploadRecord() {
-    const transaction = db.transaction(['new_tracker'], 'readwrite');
-    const trackerObjectStore = transaction.objectStore('new_tracker');
-    const getAll = trackerObjectStore.getAll();
+  const transaction = db.transaction(['new_tracker'], 'readwrite');
+  const trackerObjectStore = transaction.objectStore('new_tracker');
+  const getAll = trackerObjectStore.getAll();
 
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-                    const transaction = db.transaction(['new_tracker'], 'readwrite');
-                    const trackerObjectStore = transaction.objectStore('new_tracker');
-                    trackerObjectStore.clear();
-                    alert('All saved transaction has been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
         }
-    };
+      })
+      .then(response => response.json())
+      .then(serverResponse => {
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+        const transaction = db.transaction(['new_tracker'], 'readwrite');
+        const trackerObjectStore = transaction.objectStore('new_tracker');
+        trackerObjectStore.clear();
+        alert('All saved transaction has been submitted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  };
 }
 
 window.addEventListener('online', uploadRecord);
